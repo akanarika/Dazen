9fdcaba0d34eceb43327aeb83f2c646fccf4a453
akanarika <ilykn@hotmail.com>
2015-03-31 16:39:27 +0800

    animations added

--- play.js
+++ play.js
@@ -142,6 +142,8 @@ Game.Play.prototype = {
 	add_score: function(_player,_item) {
 		score++;
 		_item.kill();
+		player.animations.play('happy');
+		bird.animations.play('happy');
 	},
 
 	render: function() {
